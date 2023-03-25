50d1bac4f5c0363028daa79a35820e9d28562b22
Risha Sringa Changmai <michael.the.don@gmail.com>
2023-03-25 23:14:36 +0530

    more fixes

--- sketch.js
+++ sketch.js
@@ -19,12 +19,13 @@ import {drawMountains} from "./lib/mountain.js";
 import {drawClouds} from "./lib/clouds.js";
 import {drawTree} from "./lib/tree.js";
 import {drawCollectable} from "./lib/collectable.js";
-import {checkIfGameHasWon, drawGameScore, drawGameStates} from "./lib/score.js";
+import {checkIfGameHasWon, drawGameStates, drawScoreBoard} from "./lib/score.js";
 import {checkFlagpole, drawFlagPole} from "./lib/flagpole.js";
-import {checkLives, drawLives} from "./lib/lives.js";
+import {checkLives} from "./lib/lives.js";
 import {
 	characterIsFalling,
 	characterIsJumping,
+	characterIsPlummeting,
 	moveLeft,
 	movements,
 	moveRight,
@@ -49,6 +50,7 @@ function preload() {
 		jump: loadSound('assets/jump.wav'),
 		hit: loadSound('assets/hit.wav'),
 		collect: loadSound('assets/collect.wav'),
+		finished: loadSound('assets/finished.wav')
 	}
 }
 
@@ -91,17 +93,15 @@ function draw() {
 
 	state.enemies.forEach(item => item.draw())
 
-	drawGameScore(appState.game_score)
-
 	drawFlagPole(appState.flagpole, appState.floorPos_y)
 
 	checkFlagpole(appState)
 
-	drawLives(appState)
-
-	checkIfGameHasWon(appState)
+	checkIfGameHasWon(appState, sounds)
 
 	drawGameStates(appState)
+
+	drawScoreBoard(appState)
 
 	// Draw the platforms
 	state.platforms.forEach(item => item.draw())
@@ -116,11 +116,12 @@ function draw() {
 	if (state) {
 		movements(state, stickman, walkDelta)
 	}
+
+	checkLives(state)
 	pop()
 
-	//console.log(state.isFalling)
-
 	///////////INTERACTION CODE//////////
+
 	if (state.isLeft) {
 		// move the game character to the left
 		// state.gameChar_x -= walkDelta
@@ -132,19 +133,6 @@ function draw() {
 		// state.gameChar_x += walkDelta
 		moveRight(state, walkDelta, state.platforms)
 	}
-
-	/*if (state.isFalling && (state.isLeft || state.isRight)) {
-		// move the game character up
-		commit(state, {gameChar_y: state.gameChar_y - jumpDelta})
-	} else if (state.isFalling && state.gameChar_y < state.floorPos_y) {
-		// move the game character down
-		// state.gameChar_y += jumpDelta
-		commit(state, {gameChar_y: state.gameChar_y + jumpDelta})
-		if (state.gameChar_y === state.floorPos_y) {
-			// character has landed
-			commit(state, {isFalling: false})
-		}
-	}*/
 	if (state.isFalling) {
 		// character is falling
 		//state.gameChar_y -= jumpDelta
@@ -155,54 +143,32 @@ function draw() {
 	}
 
 	// if the collectable item has been found
-	state.collectables.forEach(collectable => {
-		if (dist(state.gameChar_x, state.gameChar_y, collectable.x_pos, collectable.y_pos) < 50) {
-			if (!collectable.isFound) {
-				sounds.collect.play()
-				collectable.isFound = true
-				commit(state, {game_score: state.game_score + 1})
-				//state.game_score += 1
+	if (!state.gameOver) {
+		state.collectables.forEach(collectable => {
+			if (dist(state.gameChar_x, state.gameChar_y, collectable.x_pos, collectable.y_pos) < 50) {
+				if (!collectable.isFound) {
+					sounds.collect.play()
+					collectable.isFound = true
+					commit(state, {game_score: state.game_score + 1})
+					//state.game_score += 1
+				}
 			}
-		}
-	})
+		})
+	}
 	// if the game character is over the canyon it falls to death
 	state.canyons.forEach(canyon => {
 		if (state.gameChar_x >= (canyon.x_pos) && state.gameChar_x <= (canyon.x_pos + canyon.width) && !state.isFalling) {
-			// state.isPlummeting = true
+			if (!state.isPlummeting) {
+				commit(appState, {lives: appState.lives - 1})
+			}
 			commit(state, {isPlummeting: true})
 		}
 	})
 
 	// if the game character has fallen to death, reset the game
 	if (state.isPlummeting) {
-		state.isFalling = false
-		state.gameChar_y += jumpDelta
-		if (state.gameChar_y > height) {
-			// reset the game
-			checkLives(state)
-			commit(state, {isLeft: false, isRight: false, isFalling: false, isPlummeting: false, gameChar_y: state.floorPos_y, gameChar_x: width/2, cameraPosX: 0})
-		}
+		characterIsPlummeting(state)
 	}
-
-	// No loop needed if we use keyIsDown
-	/*if (keyIsDown(LEFT_ARROW)) {
-		if (state.isFalling && state.gameChar_y < state.floorPos_y) {
-			if (keyIsDown(UP_ARROW)) {
-				state.cameraPosX -= walkDelta
-			}
-		} else {
-			state.cameraPosX -= walkDelta
-		}
-	}
-	if (keyIsDown(RIGHT_ARROW)) {
-		if (state.isFalling && state.gameChar_y < state.floorPos_y) {
-			if (keyIsDown(UP_ARROW)) {
-				state.cameraPosX += walkDelta
-			}
-		} else {
-			state.cameraPosX += walkDelta
-		}
-	}*/
 }
 
 function keyPressed() {
